ab148def8321b4c926ac95baa35996b99e224038
jayn95 <126637904+jayn95@users.noreply.github.com>
2025-11-24 21:33:34 +0800

    added libraries

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from gradio_client import Client, handle_file
 import tempfile, base64, os, threading
 from flask_cors import CORS
+import numpy as np
+import cv2
 
 app = Flask(__name__)
 CORS(app)
@@ -178,3 +180,4 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
